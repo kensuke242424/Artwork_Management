9cf58d96b4bccbc99e717beb8a977f11433968c7
kensuke242424 <kennsuke242424@gmail.com>
2022-09-24 19:46:23 +0900

    ItemViewModel_add.

---
+++ Artwork_Management/ViewModels/ItemViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ItemViewModel.swift
+//  Artwork_Management
+//
+//  Created by 中川賢亮 on 2022/09/24.
+//
+
+import Foundation
+
+class ItemViewModel: ObservableObject {
+
+    @Published var ItemsList: [Item] = []
+
+}
